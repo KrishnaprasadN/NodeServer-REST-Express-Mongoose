d19d42e6a88a99044425524bd04776acd22f2594
KrishnaprasadN <krishna.prasad@robosoftin.com>
2020-07-17 20:08:57 +0530

    Nested schema is handled with Mongoose.

--- index.js
+++ index.js
@@ -8,26 +8,40 @@ const connect = mongoose.connect(url)
 connect.then((db) => {
     console.log('** Connect correctly to server')
 
-    var newDish = Dishes({
-        name: "Dosa",
-        description: "new dosa"
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'new Pizza'
     })
+    .then((dish) => {
+        console.log(dish);
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish)
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated Pizza'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
 
-            return Dishes.find({}).exec()
-        })
-        .then((dishes) => {
-            console.log(dishes)
+        dish.comments.push({
+            rating: 5,
+            comment: 'Pizza is really Good',
+            author: 'Krishna Prasad'
+        });
 
-            return Dishes.remove({})
-        })
-        .then(() => {
-            return mongoose.connection.close()
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 })
