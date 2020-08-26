ed247f4b2e69a245eee5b88714fc3eab0e23b876
KrishnaprasadN <krishna.prasad@robosoftin.com>
2020-08-26 17:24:54 +0530

    Basic Authentication is implemented.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const url = 'mongodb://localhost:27017/conFusion'
 const connect = mongoose.connect(url)
 connect.then((db) => {
   console.log('*** Connected to DB')
-}, (err) => console.log('*** Error connectin to DB - ' , err))
+}, (err) => console.log('*** Error connectin to DB - ', err))
 
 
 var app = express();
@@ -32,6 +32,35 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// authorization function which checks for user name and password in the
+// header as a basic authentication method
+function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+    next(); // authorized
+  } else {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth)
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -41,12 +70,12 @@ app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
