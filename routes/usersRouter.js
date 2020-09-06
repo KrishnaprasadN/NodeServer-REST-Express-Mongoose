2d33850cdbe6e1f226b0f109b51b4015d3a01351
KrishnaprasadN <krishna.prasad@robosoftin.com>
2020-09-06 19:00:39 +0530

    Mongoose Population is used to join the comments and author schema. Comments schema has a reference to user id as author

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -23,10 +23,26 @@ router.post('/signup', (req, res, next) => {
         res.setHeader('Content-Type', 'application/json');
         res.json({ err: err });
       } else {
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({ success: true, status: 'Registration Successful!' });
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
+        }
+
+        if (req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ success: true, status: 'Registration Successful!' });
+          });
         });
       }
     });
@@ -37,11 +53,11 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   // create a json web token and send it back in the response
   // this token in created using user id as payload. 
   // In general we can use any unique user information for user verification
-  var token = authenticate.getToken({_id: req.user._id, name: req.user.username});
+  var token = authenticate.getToken({ _id: req.user._id, name: req.user.username });
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
 
 router.get('/logout', (req, res) => {
